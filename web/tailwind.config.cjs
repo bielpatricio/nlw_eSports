83ac4b71c0b219ce88953ef36dac4bee08b06d0a
Gabriel <gabriel.patricio@pbhub.com.br>
2022-09-14 00:52:09 -0300

    finishing the styles from web and mobile

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx', './index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      backgroundImage: {
+        galaxy: "url('background.png')",
+        'nlw-gradient':
+          'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 66.94%, #E1D55D 27.57%)',
+        'game-gradient':
+          'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      },
+    },
+  },
+  plugins: [],
+}
